3d838df09a93b04ff308376b35d0969f023e4774
Julian Oliveira Campos <julian.ocampos@hotmail.com>
2024-02-20 12:29:19 -0300

    developed struct to receive and response http requests

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,5 +1,22 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
 
-func CreateUser(c *gin.Context) {}
+	"github.com/gin-gonic/gin"
+	"github.com/juliancampos/crud_mvc_go/src/configuration/rest_err"
+	"github.com/juliancampos/crud_mvc_go/src/controller/model/request"
+)
+
+func CreateUser(c *gin.Context) {
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("Some incorret fields, error=%s\n", err.Error()))
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
+}
